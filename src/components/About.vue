06713f1c78e3c96f52626587faa5d830680d0468
liqinkang <liqinkang@truxing.com>
2017-09-26 16:02:35 +0800

    更新readme，更新关于

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,8 +2,10 @@
   <Layout :has_menu="false" :has_footer="false" title="关于我">
     <div class="wrap">
         <h4>基于vue2.0开发</h4>
-        <h5>最新最好看的电影APP</h5>
-        <h6>开源项目：<a href="https://github.com/kang558/vue-movie">GitHub</a></h6>
+        <h5>最新最好看的在线电影APP</h5>
+        <h6>使用了：<a href="http://www.muse-ui.org/#/index" target="_blank">muse-ui</a></h6>
+        <h6>开源项目：<a href="https://github.com/kang558/vue-movie" target="_blank">GitHub</a></h6>
+        <h6>版权声明：<span style="color: #999;font-weight:300">api来源于网上，仅供开发参考之用，禁用于商业，否则后果自负。</span></h6>
     </div>
   </Layout>
 </template>
@@ -40,7 +42,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   .wrap {
-    text-align: center;
+    padding: 0 20px;
+    text-align: left;
     padding-top: 30px;
+
   }
 </style>
